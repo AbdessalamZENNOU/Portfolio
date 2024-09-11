ee45eca55d3dee3f301021a9be450e36184f2147
Abdessalam ZENNOU <107582950+ZaiimZinu@users.noreply.github.com>
2024-09-11 15:28:37 +0100

    env v

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,8 +1,8 @@
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend("re_Uf9CuFz3_FQPjQkL53eovDRNujW5WpuhL");
-const fromEmail = "abdessalam.zennou@gmail.com";
+const resend = new Resend(process.env.RESEND_API_KEY);
+const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
   const { email, subject, message } = await req.json();
